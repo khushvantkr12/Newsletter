274ec6645216ba47ea763e71b14ce9bc92e5ba61
khushvantkr12 <98275946+khushvantkr12@users.noreply.github.com>
2023-06-02 11:06:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.post("/", function(req, res){
 
     const options = {
         method: "POST",
-        auth: "kittu3:8fe1fff09c10fb27283e765049ece4b0-us11"
+        auth: "kittu3:6374f5442987415f43a90f6cd9c8e5f4-us11"
     };
 
     // *** Requesting and send back our data to mailchimp ***
